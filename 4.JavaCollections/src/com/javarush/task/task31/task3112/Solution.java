01389d05f4783388dd15d86bd0943e77a56417e2
Dmitriy Shelemekh <d.shelemekh@gmail.com>
2017-10-07 04:17:24 +0300

    Добавил задачки

--- 4.JavaCollections/src/com/javarush/task/task31/task3112/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task31/task3112/Solution.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task31.task3112;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -10,9 +11,10 @@ import java.nio.file.Paths;
 Загрузчик файлов
 */
 public class Solution {
-
     public static void main(String[] args) throws IOException {
-        Path passwords = downloadFile("https://www.amigo.com/ship/secretPassword.txt", Paths.get("D:/MyDownloads"));
+        //Path passwords = downloadFile("http://placehold.it/100x50/88cc00.png", Paths.get("e:\\a.txt"));
+        Path passwords = downloadFile("https://yastatic.net/morda-logo/i/citylogos/yandex19-logo-ru.png",Paths.get("E:/"));
+        //Path passwords = downloadFile("https://www.amigo.com/ship/secretPassword.txt", Paths.get("E:\\"));
 
         for (String line : Files.readAllLines(passwords)) {
             System.out.println(line);
@@ -21,6 +23,16 @@ public class Solution {
 
     public static Path downloadFile(String urlString, Path downloadDirectory) throws IOException {
         // implement this method
-        return null;
+        URL url=new URL(urlString);
+        InputStream inputStream=url.openStream();
+
+        Path tmp=Files.createTempFile("temp-",".tmp");
+        Files.copy(inputStream,tmp);
+
+        String fieName=urlString.substring(urlString.lastIndexOf("/"));
+        Path destPath=Paths.get(downloadDirectory.toString(), fieName);
+        Files.move(tmp,destPath);
+
+        return destPath;
     }
 }
