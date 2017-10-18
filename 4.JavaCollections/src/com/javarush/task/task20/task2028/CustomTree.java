6b38fba85cac950e9894acbaa525f2b68671ee9c
Dmitriy Shelemekh <d.shelemekh@gmail.com>
2017-10-18 22:42:28 +0300

    Добавил задачки

--- 4.JavaCollections/src/com/javarush/task/task20/task2028/CustomTree.java
+++ 4.JavaCollections/src/com/javarush/task/task20/task2028/CustomTree.java
@@ -1,152 +1,272 @@
 package com.javarush.task.task20.task2028;
 
 import java.io.Serializable;
-import java.util.AbstractList;
-import java.util.List;
-import java.util.Queue;
-import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.*;
 
-/* 
+/*
 Построй дерево(1)
 */
 public class CustomTree extends AbstractList<String> implements Cloneable, Serializable {
+
+    private Entry<String> root;
+
+    public CustomTree() {
+        root = new Entry<>("0");
+    }
+
     public static void main(String[] args) {
         List<String> list = new CustomTree();
         for (int i = 1; i < 16; i++) {
             list.add(String.valueOf(i));
         }
-        //System.out.println("Expected 3, actual is " + ((CustomTree) list).getParent("8"));
-        list.remove("5");
-        //System.out.println("Expected null, actual is " + ((CustomTree) list).getParent("11"));
+        System.out.println("Size: Expected 15, actual is " + list.size());
+        System.out.println("Parent: Expected NOT PARENT, actual is " + ((CustomTree) list).getParent("0"));
+        System.out.println("Parent: Expected 0, actual is " + ((CustomTree) list).getParent("1"));
+        System.out.println("Parent: Expected 1, actual is " + ((CustomTree) list).getParent("3"));
+        System.out.println("Parent: Expected 1, actual is " + ((CustomTree) list).getParent("4"));
+        System.out.println("Parent: Expected 2, actual is " + ((CustomTree) list).getParent("5"));
+        System.out.println("Parent: Expected 3, actual is " + ((CustomTree) list).getParent("7"));
+        System.out.println("Parent: Expected 4, actual is " + ((CustomTree) list).getParent("9"));
+        System.out.println("Parent: Expected 5, actual is " + ((CustomTree) list).getParent("11"));
+        System.out.println("Parent: Expected 6, actual is " + ((CustomTree) list).getParent("13"));
+        System.out.println("Parent: Expected 7, actual is " + ((CustomTree) list).getParent("15"));
+        System.out.println("Parent: Expected null, actual is " + ((CustomTree) list).getParent("20"));
+        System.out.println("Remove: element 15: " + list.remove("15"));
+        //System.out.println("Remove: element 3: " + list.remove("3"));
+        System.out.println("Size: Expected 14, actual is " + list.size());
+        //list.add("16");
+
+        System.out.println();
+        System.out.println("Общий вид дерева:");
+        System.out.println(list);
     }
 
-    Entry<String> root = new Entry<>("0");
+    @Override
+    public boolean add(String s) {
+        if (s == null)
+            return false;
+        Queue<Entry<String>> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+
+            // Тут было решение, если вкратце, то нужно брать элемент из очереди, проверять его потомков,
+            // и если какого-то нет, то ставить на его место новый элемент.
+            // Если все потомки есть, то добавлять их в очередь и делать тоже самое с ними
+            Entry<String> rootElement = queue.poll();
+            if(rootElement!=null) {
+                if (rootElement.isAvailableToAddChildren()) {
+                    if (rootElement.availableToAddLeftChildren) {
+                        rootElement.leftChild = new Entry<>(s);
+                        rootElement.leftChild.parent = rootElement;
+                    } else if (rootElement.availableToAddRightChildren) {
+                        rootElement.rightChild = new Entry<>(s);
+                        rootElement.rightChild.parent = rootElement;
+                    }
+                    return true;
+                } else {
+                    queue.offer(rootElement.leftChild);
+                    queue.offer(rootElement.rightChild);
+                }
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        if (o == null)
+            return false;
+        String s = (String) o;
+        if (s.equals(root.elementName)) {
+            root = new Entry<>("0");
+            return true;
+        }
+        Queue<Entry<String>> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            //Удаление элемента похоже на добавление,
+            // надо проверять потомков у текущего элемента из очереди и обнулять потомка с искомым именем
+            Entry<String> rootElement = queue.poll();
+            if (rootElement.leftChild != null && rootElement.leftChild.elementName.equals(s)) {
+                //rootElement.leftChild.parent = null;
+                rootElement.leftChild = null;
+                return true;
+            } else if (rootElement.rightChild != null && rootElement.rightChild.elementName.equals(s)) {
+                //rootElement.rightChild.parent = null;
+                rootElement.rightChild = null;
+                return true;
+            } else {
+                if (rootElement.leftChild != null) {
+                    queue.offer(rootElement.leftChild);
+                }
+                if (rootElement.rightChild != null) {
+                    queue.offer(rootElement.rightChild);
+                }
+            }
+        }
+        return false;
+    }
+
+    public String getParent(String s) {
+        if (root == null || s == null)
+            return null;
+
+        Queue<Entry<String>> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            //тоже берём первый элемент из очереди, если это искомый элемент, то возвращаем имя его родителя,
+            //иначе проверяем его детей, и если они есть, то добавляем их в очередь. И всё по новой
+            Entry<String> rootElement = queue.poll();
+            if (rootElement.elementName.equals(s)) {
+                if (rootElement.parent == null)
+                    return "NOT PARENT";
+                else
+                    return rootElement.parent.elementName;
+            } else {
+                if (rootElement.leftChild != null) {
+                    queue.offer(rootElement.leftChild);
+                }
+                if (rootElement.rightChild != null) {
+                    queue.offer(rootElement.rightChild);
+                }
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public int size() {
+        Queue<Entry<String>> queue = new LinkedList<>();
+        int count = 0;
+        if (root != null) {
+            if (root.leftChild != null)
+                queue.offer(root.leftChild);
+            if (root.rightChild != null)
+                queue.offer(root.rightChild);
+        }
+        while (!queue.isEmpty()) {
+            //берём элемент из очереди, считаем его,
+            // потом добавляем в очередь существующих потомков, и всё по новой
+            Entry<String> rootElement = queue.poll();
+            count++;
+            if (rootElement.leftChild != null) {
+                queue.offer(rootElement.leftChild);
+            }
+            if (rootElement.rightChild != null) {
+                queue.offer(rootElement.rightChild);
+            }
+        }
+        return count;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        Queue<Entry<String>> queue = new LinkedList<>();
+
+        if (root != null) {
+            if (root.leftChild != null)
+                queue.offer(root.leftChild);
+            if (root.rightChild != null)
+                queue.offer(root.rightChild);
+        }
+        while (!queue.isEmpty()) {
+            //берём элемент из очереди, добавляем значение его метода toString() в sb,
+            // потом добавляем в очередь существующих потомков, и всё по новой
+            Entry<String> rootElement = queue.poll();
+            sb.append(rootElement.toString());
+            if (rootElement.leftChild != null) {
+                queue.offer(rootElement.leftChild);
+            }
+            if (rootElement.rightChild != null) {
+                queue.offer(rootElement.rightChild);
+            }
+        }
+        return sb.toString();
+    }
 
     static class Entry<T> implements Serializable {
+
         String elementName;
-        int lineNumber = 0;
-        boolean availableToAddLeftChildren,
-                availableToAddRightChildren;
-        Entry<T> parent,
-                leftChild,
-                rightChild;
+        int lineNumber;
+        boolean availableToAddLeftChildren, availableToAddRightChildren;
+        Entry<T> parent, leftChild, rightChild;
 
         public Entry(String elementName) {
             this.elementName = elementName;
-            this.availableToAddLeftChildren = true;
-            this.availableToAddRightChildren = true;
+            availableToAddLeftChildren = true;
+            availableToAddRightChildren = true;
         }
 
         void checkChildren() {
             if (leftChild != null)
                 availableToAddLeftChildren = false;
+
             if (rightChild != null)
                 availableToAddRightChildren = false;
         }
 
-        public boolean isAvailableToAddChildren() {
-            return availableToAddRightChildren || availableToAddLeftChildren;
+        boolean isAvailableToAddChildren() {
+            checkChildren();
+            return availableToAddLeftChildren | availableToAddRightChildren;
         }
-    }
 
-    public boolean add(String element) {
-        Entry<String> newElement = new Entry<>(element);
-        Queue<Entry<String>> queue = new ConcurrentLinkedQueue<>();
-        queue.add(root);
-        while (true) {
-            Entry<String> currentRoot = queue.remove();
-            // если находим локальный "корень" у которого нет 1 или 2=х "детей", то записываем
-            if (currentRoot.isAvailableToAddChildren()) {
-                elementAdd(currentRoot, newElement);
-                break;
-            } // если дети есть, то ставим их в очередь
-            else {
-                if (currentRoot.leftChild != null)
-                    queue.add(currentRoot.leftChild);
-                if (currentRoot.rightChild != null)
-                    queue.add(currentRoot.rightChild);
+        @Override
+        public String toString() {
+
+            String parent = elementName;
+
+            String left = "null";
+            if (leftChild != null) {
+                left = leftChild.elementName;
             }
+
+            String right = "null";
+            if (rightChild != null) {
+                right = rightChild.elementName;
+            }
+
+            return String.format("%s {%s %s}%n", parent, left, right);
         }
-        return true;
     }
 
     @Override
-    public String get(int index) {
-        return null;
-    }
-
-    //проверяет наличие детей и запихивает туда, где их нет
-    public void elementAdd(Entry<String> currentRoot, Entry<String> currentElement) {
-        if (currentRoot.availableToAddLeftChildren) {
-            currentElement.parent = currentRoot;
-            currentElement.lineNumber = currentRoot.lineNumber + 1;
-            currentRoot.leftChild = currentElement;
-        } else if (currentRoot.availableToAddRightChildren) {
-            currentElement.parent = currentRoot;
-            currentElement.lineNumber = currentRoot.lineNumber + 1;
-            currentRoot.rightChild = currentElement;
-        }
-        currentRoot.checkChildren();
+    public String get(int index) throws UnsupportedOperationException {
+        throw new UnsupportedOperationException();
     }
 
     @Override
-    public boolean remove(Object object) {
-        String removeElementStr = (String) object;
-        //ищем нужный элемент в графе и если он не null, удаляем
-        Entry<String> removeElement = findElement(root, removeElementStr);
-        if (removeElement != null) {
-            if (removeElement.parent.leftChild != null && removeElement.parent.leftChild.elementName.equals(removeElement.elementName))
-                removeElement.parent.leftChild = null;
-            else removeElement.parent.rightChild = null;
-            removeElement.parent = null;
-        }
-        return true;
-    }
-
-    //метод ищет указанный элемент в графе и взвращает его или null, если его нет
-    public Entry<String> findElement(Entry<String> root, String findElement) {
-        Entry<String> elementOut = null;
-        Queue<Entry<String>> queue = new ConcurrentLinkedQueue<>();
-        queue.add(root);
-        while (queue.size() != 0) {
-            Entry<String> currentElement = queue.remove();
-            if (currentElement.elementName.equals(findElement)) {
-                elementOut = currentElement;
-                break;
-            } else {
-                if (currentElement.leftChild != null)
-                    queue.add(currentElement.leftChild);
-                if (currentElement.rightChild != null)
-                    queue.add(currentElement.rightChild);
-            }
-        }
-        return elementOut;
+    public void add(int index, String element) throws UnsupportedOperationException {
+        throw new UnsupportedOperationException();
     }
 
     @Override
-    public int size() {
-        int size = 1;
-        Queue<Entry<String>> queue = new ConcurrentLinkedQueue<>();
-        queue.add(root);
-        // пробегаемся по элементам
-        while (queue.size() != 0) {
-            Entry<String> currentElement = queue.remove();
-            if (currentElement.leftChild != null) {
-                queue.add(currentElement.leftChild);
-                size++;
-            }
-            if (currentElement.rightChild != null) {
-                queue.add(currentElement.rightChild);
-                size++;
-            }
-        }
-        return size;
+    public String set(int index, String element) throws UnsupportedOperationException {
+        throw new UnsupportedOperationException();
     }
 
-    public String getParent(String element) {
-        Entry<String> findElement = findElement(root, element);
-        if (findElement != null)
-            return findElement.parent.elementName;
-        else return null;
+    @Override
+    public String remove(int index) throws UnsupportedOperationException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends String> c) throws UnsupportedOperationException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public List<String> subList(int fromIndex, int toIndex) throws UnsupportedOperationException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    protected void removeRange(int fromIndex, int toIndex) throws UnsupportedOperationException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
     }
 }
