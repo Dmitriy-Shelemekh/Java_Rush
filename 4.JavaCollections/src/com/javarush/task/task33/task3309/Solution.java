4623e6016b2f081f909309ae1c0d4bffbaed892e
Шелемех Дмитрий Дмитриевич <SheDD@bss.lan>
2017-10-18 17:41:53 +0300

    Добавил задачки

--- 4.JavaCollections/src/com/javarush/task/task33/task3309/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task33/task3309/Solution.java
@@ -1,48 +1,64 @@
 package com.javarush.task.task33.task3309;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
 import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
-import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlType;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import java.io.StringWriter;
 
-/* 
+/*
 Комментарий внутри xml
 */
 public class Solution {
     public static String toXmlWithComment(Object obj, String tagName, String comment) {
-        StringWriter writer = new StringWriter();
-        String res = null;
         try {
             JAXBContext context = JAXBContext.newInstance(obj.getClass());
             Marshaller marshaller = context.createMarshaller();
             marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
-            marshaller.marshal(obj, writer);
-
-            String xml = writer.toString();
-
-            if (xml.indexOf(tagName) > -1)
-                res = xml.replace("<" + tagName + ">", "<!--" + comment + "-->\n" + "<" + tagName + ">");
-            else
-                res = xml;
-
-        } catch (JAXBException e) {
-            e.printStackTrace();
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document document = builder.newDocument();
+            document.setXmlStandalone(false);
+            marshaller.marshal(obj, document);
+            changeTextToCDATA(document, document);
+            NodeList nodeList = document.getElementsByTagName(tagName);
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                Node element = nodeList.item(i);
+                element.getParentNode().insertBefore(document.createComment(comment), element);
+            }
+            StringWriter sw = new StringWriter();
+            TransformerFactory tf = TransformerFactory.newInstance();
+            Transformer transformer = tf.newTransformer();
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+            transformer.transform(new DOMSource(document), new StreamResult(sw));
+            return sw.toString();
+        } catch (Exception e) {
+            throw new RuntimeException();
         }
-        return res;
     }
 
-//    public static void main(String[] args) {
-//        String s = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?><first><second>some string</second><second>some string</second><second><![CDATA[need CDATA because of < and >]]></second><second/></first>";
-//
-//        String comment = "it's a comment";
-//        String tagName = "second";
-//
-//        String res = null;
-//        if (s.indexOf(tagName) > -1)
-//            res = s.replace("<" + tagName + ">", "<!--" + comment + "-->" + "<" + tagName + ">");
-//
-//        System.out.println(res);
-//    }
+    private static void changeTextToCDATA(Node start, Document document) {
+        {
+            if (start.getNodeType() == start.TEXT_NODE) {
+                String text = start.getNodeValue();
+                if (text.matches(".*[<>&'\"].*")) {
+                    Node cdataNode = document.createCDATASection(text);
+                    start.getParentNode().replaceChild(cdataNode, start);
+                }
+            }
+            for (Node child = start.getFirstChild(); child != null; child = child.getNextSibling()) {
+                changeTextToCDATA(child, document);
+            }
+        }
+    }
 }
