21f0837783fd4fdf63bce9684ec802ef889911fa
Dmitriy Shelemekh <d.shelemekh@gmail.com>
2017-11-06 22:57:56 +0300

    Фабрики (3)

---
+++ 4.JavaCollections/src/com/javarush/task/task37/task3702/Human.java
@@ -0,0 +1,7 @@
+package com.javarush.task.task37.task3702;
+
+/**
+ * Created by Dmitriy Shelemekh on 06.11.2017.
+ */
+public interface Human {
+}
---
+++ 4.JavaCollections/src/com/javarush/task/task37/task3702/male/MaleFactory.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task37.task3702.male;
+
+import com.javarush.task.task37.task3702.Human;
+
+/**
+ * Created by Dmitriy Shelemekh on 06.11.2017.
+ */
+
+public class MaleFactory {
+    public Human getPerson(int age) {
+        if (age <= KidBoy.MAX_AGE)
+            return new KidBoy();
+        if (age <= TeenBoy.MAX_AGE)
+            return new TeenBoy();
+        return new Man();
+    }
+}
