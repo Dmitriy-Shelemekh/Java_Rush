86119af4d665559272c515f9d4d4bc9814a2927b
Шелемех Дмитрий Дмитриевич <SheDD@bss.lan>
2017-11-07 18:28:34 +0300

    Ипподром(4)

---
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task21.task2113;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Dmitry Shelemekh on 07.11.2017.
+ */
+public class Hippodrome {
+
+    private List<Horse> horses = new ArrayList<>();
+
+    public List<Horse> getHorses(){
+        return horses;
+    }
+
+    public Hippodrome(List<Horse> horses) { this.horses = horses;}
+
+    public static void main(String[] args){
+
+    }
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Horse.java
@@ -0,0 +1,7 @@
+package com.javarush.task.task21.task2113;
+
+/**
+ * Created by Dmitry Shelemekh on 07.11.2017.
+ */
+public class Horse {
+}
