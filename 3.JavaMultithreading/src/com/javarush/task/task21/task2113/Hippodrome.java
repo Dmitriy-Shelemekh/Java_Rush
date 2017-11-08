5eee48026b8175e58ab08114947fcb532aed588e
Шелемех Дмитрий Дмитриевич <SheDD@bss.lan>
2017-11-08 16:54:45 +0300

    Ипподром(10)

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
@@ -8,15 +8,42 @@ import java.util.List;
  */
 public class Hippodrome {
 
+    static Hippodrome game;
+
     private List<Horse> horses = new ArrayList<>();
 
-    public List<Horse> getHorses(){
+    public List<Horse> getHorses() {
         return horses;
     }
 
-    public Hippodrome(List<Horse> horses) { this.horses = horses;}
+    public Hippodrome(List<Horse> horses) {
+        this.horses = horses;
+    }
 
-    public static void main(String[] args){
+    public void run() throws InterruptedException {
+        for (int i = 0; i < 100; i++) {
+            move();
+            print();
+            Thread.sleep(200);
+        }
+    }
+
+    public void move() {
+        for (Horse horse : horses ) {
+            horse.move();
+        }
+    }
+
+    public void print() {
+    }
+
+    public static void main(String[] args) {
+
+        game = new Hippodrome(new ArrayList<>());
+
+        game.getHorses().add(new Horse("Horse_1", 3, 0));
+        game.getHorses().add(new Horse("Horse_2", 3, 0));
+        game.getHorses().add(new Horse("Horse_3", 3, 0));
 
     }
 }
--- 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Horse.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Horse.java
@@ -4,4 +4,42 @@ package com.javarush.task.task21.task2113;
  * Created by Dmitry Shelemekh on 07.11.2017.
  */
 public class Horse {
+
+    String name;
+    double speed;
+    double distance;
+
+    public void move() {}
+
+    public void print() {}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public void setDistance(double distance) {
+        this.distance = distance;
+    }
+
+    public Horse(String name, double speed, double distance) {
+        this.name = name;
+        this.speed = speed;
+        this.distance = distance;
+    }
 }
