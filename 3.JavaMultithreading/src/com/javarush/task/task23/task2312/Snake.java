5d58dc9c6fdf3a45b188ac1685a87a76834d1157
Шелемех Дмитрий Дмитриевич <SheDD@bss.lan>
2017-11-10 18:03:09 +0300

    Змейка(17)

--- 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Snake.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Snake.java
@@ -44,6 +44,49 @@ public class Snake {
     }
 
     public void move() {
+        if (isAlive) {
+            if (direction == SnakeDirection.UP) {
+                move(0, -1);
+            } else if (direction == SnakeDirection.RIGHT) {
+                move(1, 0);
+            } else if (direction == SnakeDirection.DOWN) {
+                move(0, 1);
+            } else if (direction == SnakeDirection.LEFT) {
+                move(-1, 0);
+            }
+        }
+    }
 
+    public void move(int dx, int dy) {
+        SnakeSection newSection = new SnakeSection(sections.get(0).getX() + dx, sections.get(0).getY() + dy);
+        checkBorders(newSection);
+        if (!isAlive) return;
+        checkBody(newSection);
+        if (!isAlive) return;
+        if (newSection.getX() == Room.game.getMouse().getX() && newSection.getY() == Room.game.getMouse().getY()) {
+            Room.game.eatMouse();
+            sections.add(0, newSection);
+        } else {
+            sections.add(0, newSection);
+            sections.remove(sections.size() - 1);
+        }
+    }
+
+    public void checkBorders(SnakeSection head) {
+        int headX = head.getX();
+        int headY = head.getY();
+        int widthRoom = Room.game.getWidth();
+        int heightRoom = Room.game.getHeight();
+
+        if (headX < 0 || headX >= widthRoom || headY < 0 || headY >= heightRoom) {
+            isAlive = false;
+        }
+        System.out.println(isAlive);
+    }
+
+    public void checkBody(SnakeSection head) {
+        if (sections.contains(head)) {
+            isAlive = false;
+        }
     }
 }
