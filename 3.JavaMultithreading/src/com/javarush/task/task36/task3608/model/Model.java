6127eda834f728a630c372215c7ba3abaf1a972e
Шелемех Дмитрий Дмитриевич <SheDD@bss.lan>
2017-11-15 15:38:37 +0300

    MVC (2)

---
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/model/Model.java
@@ -0,0 +1,9 @@
+package com.javarush.task.task36.task3608.model;
+
+/**
+ * Created by Dmitry Shelemekh on 15.11.2017.
+ */
+public interface Model {
+    ModelData getModelData();
+    void loadUsers();
+}
