6c4e5e4609baa463819ea10b5bc8f107ce10a23f
Dmitriy Shelemekh <d.shelemekh@gmail.com>
2017-11-16 21:47:52 +0300

    MVC (4)

---
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/view/UsersView.java
@@ -0,0 +1,29 @@
+package com.javarush.task.task36.task3608.view;
+
+import com.javarush.task.task36.task3608.controller.Controller;
+import com.javarush.task.task36.task3608.model.ModelData;
+
+/**
+ * Created by Dmitriy Shelemekh on 16.11.2017.
+ */
+public class UsersView implements View{
+    private Controller controller;
+
+    public void fireEventShowAllUsers() {
+        controller.onShowAllUsers();
+    }
+
+    @Override
+    public void refresh(ModelData modelData) {
+        System.out.println("All users:");
+        for (int i = 0; i < modelData.getUsers().size(); i++) {
+            System.out.println("\t" + modelData.getUsers().get(i));
+        }
+        System.out.println("===================================================");
+    }
+
+    @Override
+    public void setController(Controller controller) {
+        this.controller = controller;
+    }
+}
