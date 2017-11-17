83b306ae0ae6eb3a8bf80cadc50a145e79580292
Шелемех Дмитрий Дмитриевич <SheDD@bss.lan>
2017-11-17 19:25:26 +0300

    mvc-8

--- 3.JavaMultithreading/src/com/javarush/task/task36/task3608/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/Solution.java
@@ -1,8 +1,9 @@
 package com.javarush.task.task36.task3608;
 
 import com.javarush.task.task36.task3608.controller.Controller;
-import com.javarush.task.task36.task3608.model.Model;
 import com.javarush.task.task36.task3608.model.MainModel;
+import com.javarush.task.task36.task3608.model.Model;
+import com.javarush.task.task36.task3608.view.EditUserView;
 import com.javarush.task.task36.task3608.view.UsersView;
 
 public class Solution {
@@ -10,13 +11,17 @@ public class Solution {
         Model model = new MainModel();
         UsersView usersView = new UsersView();
         Controller controller = new Controller();
+        EditUserView editUserView = new EditUserView();
 
-        usersView.setController(controller);
         controller.setModel(model);
         controller.setUsersView(usersView);
+        controller.setEditUserView(editUserView);
 
+        editUserView.setController(controller);
+
+        usersView.setController(controller);
         usersView.fireEventShowAllUsers();
+        usersView.fireEventOpenUserEditForm(126);
         usersView.fireEventShowDeletedUsers();
-        usersView.fireEventOpenUserEditForm((long) 126);
     }
 }
