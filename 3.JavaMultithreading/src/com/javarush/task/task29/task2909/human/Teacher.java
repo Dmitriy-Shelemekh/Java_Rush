54c145c4538457970594aa6c92656b90cd232ee7
Шелемех Дмитрий Дмитриевич <SheDD@bss.lan>
2017-11-20 17:10:20 +0300

    Рефакторинг

--- 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Teacher.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Teacher.java
@@ -1,10 +1,7 @@
 package com.javarush.task.task29.task2909.human;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Teacher extends Human {
-    private List<Human> children = new ArrayList<>();
+    //    private List<Human> children = new ArrayList<>();
     private int numberOfStudents;
     private String university;
 
@@ -15,13 +12,13 @@ public class Teacher extends Human {
         this.numberOfStudents = numberOfStudents;
     }
 
-    public List<Human> getChildren() {
-        return children;
-    }
+//    public List<Human> getChildren() {
+//        return children;
+//    }
 
-    public void setChildren(List<Human> children) {
-        this.children = children;
-    }
+//    public void setChildren(List<Human> children) {
+//        this.children = children;
+//    }
 
     public void live() {
         teach();
