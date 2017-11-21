a00e5d27fed1f20377405657c86b1d0f545973e0
Dmitriy Shelemekh <d.shelemekh@gmail.com>
2017-11-21 21:59:33 +0300

    Рефакторинг

--- 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Human.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Human.java
@@ -5,18 +5,21 @@ import java.util.Collections;
 import java.util.List;
 
 public class Human implements Alive {
-    public static final int FIRST = 1;
-    public static final int SECOND = 2;
-    public static final int THIRD = 3;
-    public static final int FOURTH = 4;
-    public static int nextId = 0;
-
+    private static int nextId = 0;
     protected int age;
     protected String name;
-    protected int[] size;
+    protected Size size;
     private int id;
-    private int bloodGroup;
     private List<Human> children = new ArrayList<>();
+    private BloodGroup bloodGroup;
+
+    public BloodGroup getBloodGroup() {
+        return bloodGroup;
+    }
+
+    public void setBloodGroup(BloodGroup bloodGroup) {
+        this.bloodGroup = bloodGroup;
+    }
 
     public Human(String name, int age) {
         this.name = name;
@@ -26,14 +29,6 @@ public class Human implements Alive {
     public Human(boolean isSoldier) {
         this.id = nextId;
         nextId++;
-    }
-
-    public int getBloodGroup() {
-        return bloodGroup;
-    }
-
-    public void setBloodGroup(int code) {
-        bloodGroup = code;
     }
 
     public int getAge() {
@@ -71,12 +66,8 @@ public class Human implements Alive {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
     public void printSize() {
-        System.out.println("Рост: " + size[0] + " Вес: " + size[1]);
+        System.out.println("Рост: " + size.height + " Вес: " + size.weight);
     }
 
     public String getPosition() {
@@ -85,5 +76,11 @@ public class Human implements Alive {
 
     public void printData() {
         System.out.println(getPosition() + ": " + name);
+    }
+
+    public class Size {
+
+        public int height;
+        public int weight;
     }
 }
