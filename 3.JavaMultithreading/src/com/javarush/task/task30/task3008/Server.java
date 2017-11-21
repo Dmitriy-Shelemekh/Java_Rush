2eed08e03a7ad782130e91197473b03c9b179a2a
Шелемех Дмитрий Дмитриевич <SheDD@bss.lan>
2017-11-21 16:51:31 +0300

    Чат(6)

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -1,9 +1,42 @@
 package com.javarush.task.task30.task3008;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * Created by Dmitry Shelemekh on 21.11.2017.
  */
 public class Server {
     public static void main(String[] args) {
+
+        ConsoleHelper.writeMessage("Введите порт сервера: ");
+        int serverPort = ConsoleHelper.readInt();
+
+        try (ServerSocket serverSocket = new ServerSocket(serverPort)) {
+
+            ConsoleHelper.writeMessage("Сервер запущен");
+
+            while (true) {
+                //Слушаем
+                Socket socket = serverSocket.accept();
+                Handler handler = new Handler(socket);
+                //запускаем handler
+                handler.start();
+            }
+        } catch (IOException e) {
+
+        }
+    }
+
+
+    private static class Handler extends Thread {
+        private Socket socket;
+
+        //Constructor
+        public Handler(Socket socket) {
+
+            this.socket = socket;
+        }
     }
 }
