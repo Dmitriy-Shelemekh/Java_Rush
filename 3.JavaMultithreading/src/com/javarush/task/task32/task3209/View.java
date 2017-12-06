ef5f17ffdd96e4b6f726ed33d37375bab99648d3
Шелемех Дмитрий Дмитриевич <SheDD@bss.lan>
2017-12-06 17:57:53 +0300

    HTML Editor (1)

---
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task32.task3209;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Created by Dmitry Shelemekh on 06.12.2017.
+ */
+
+public class View extends JFrame implements ActionListener {
+
+    private Controller controller;
+
+    public Controller getController() {
+        return controller;
+    }
+
+    public void setController(Controller controller) {
+        this.controller = controller;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
