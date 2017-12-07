f18708b4295b8491b03bcda0f04e42e6f3102240
Шелемех Дмитрий Дмитриевич <SheDD@bss.lan>
2017-12-07 11:08:39 +0300

    task2902 done

--- 3.JavaMultithreading/src/com/javarush/task/task29/task2902/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2902/Solution.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task29.task2902;
 
 import java.io.IOException;
+import java.net.URI;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -9,19 +10,20 @@ import java.nio.file.Paths;
 */
 public class Solution {
     public static void main(String[] args) throws IOException, InterruptedException {
-        Solution Solution = new Solution();
-        String file_name_to_be_opened_by_notepad = Solution.Getabsolutepathtodefaulttxtfile().toString();
-        Process NOTEPAD = Solution.getprocessstartnotepad(file_name_to_be_opened_by_notepad);
-        NOTEPAD.waitFor();
+        Solution solution = new Solution();
+        String fileNameToBeOpenedByNotepad = solution.getAbsolutePathToDefaultTxtFile().toString();
+        Process notepad = solution.getProcessStartNotepad(fileNameToBeOpenedByNotepad);
+        notepad.waitFor();
     }
 
-    public Process getprocessstartnotepad(String FILE_NAME) throws IOException {
-        String[] cmd_array = new String[]{"notepad.exe", FILE_NAME};
-        return Runtime.getRuntime().exec(cmd_array);
+    public Process getProcessStartNotepad(String fileName) throws IOException {
+        String[] cmdArray = new String[]{"notepad.exe", fileName};
+        return Runtime.getRuntime().exec(cmdArray);
     }
 
-    public Path Getabsolutepathtodefaulttxtfile() {
-        URI uRi = Solution.class.getResource("file.txt").toURI();
-        return  Paths.get(uRi);
+    public Path getAbsolutePathToDefaultTxtFile() {
+        try {URI uri = Solution.class.getResource("file.txt").toURI();
+            return  Paths.get(uri);} catch (Exception e) {}
+        return null;
     }
 }
