0950c368eeccbf1d63bb0418d20f1bf6785d5e14
Dmitriy Shelemekh <d.shelemekh@gmail.com>
2017-12-11 17:55:00 +0300

    task3009 done

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3009/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3009/Solution.java
@@ -9,9 +9,29 @@ import java.util.Set;
 
 public class Solution {
     public static void main(String[] args) {
+        System.out.println(getRadix("1A"));
         System.out.println(getRadix("112"));        //expected output: [3, 27, 13, 15]
         System.out.println(getRadix("123"));        //expected output: [6]
+        System.out.println(getRadix("12641"));
         System.out.println(getRadix("5321"));       //expected output: []
-        System.out.println(getRadix("1A"));         //expected output: []
+        //expected output: []
     }
+
+    private static Set<Integer> getRadix(String number) {
+        Set<Integer> result = new HashSet<>();
+        StringBuilder temp;
+        try {
+            int numberInDex = Integer.parseInt(number);
+            for (int i = 2; i < 37; i++) {
+                temp = new StringBuilder(Integer.toString(numberInDex, i));
+                if (temp.toString().equals(temp.reverse().toString())) result.add(i);
+            }
+
+        } catch (NumberFormatException e) {
+        }
+
+        return result;
+    }
+
+
 }
