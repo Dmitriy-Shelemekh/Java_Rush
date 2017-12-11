a20508c0dcb24fa7addc9cf99b4c632c002ea317
Dmitriy Shelemekh <d.shelemekh@gmail.com>
2017-12-11 17:53:56 +0300

    task3002 done

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3002/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3002/Solution.java
@@ -10,10 +10,25 @@ public class Solution {
         System.out.println(convertToDecimalSystem("012"));  //10
         System.out.println(convertToDecimalSystem("0b10")); //2
         System.out.println(convertToDecimalSystem("62"));   //62
+
     }
 
+
     public static String convertToDecimalSystem(String s) {
-        //напишите тут ваш код
-        return s;
+        int i = 0;
+        if (!s.startsWith("0")) return s;
+        else {
+            if (s.startsWith("0x")) i = Integer.parseInt(s.substring(2), 16);
+            else {
+                if (s.startsWith("0b")) i = Integer.parseInt(s.substring(2), 2);
+                else {
+                    if (s.startsWith("0")) i = Integer.parseInt(s, 8);
+
+                }
+            }
+        }
+
+
+        return String.valueOf(i);
     }
 }
