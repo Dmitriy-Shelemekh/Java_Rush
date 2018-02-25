ddf6fecd95509f22d02ffb6a14744f7d4c1c308c
Dmitriy Shelemekh <d.shelemekh@gmail.com>
2018-02-25 23:10:46 +0300

    Рефакторинг

--- 1.JavaSyntax/src/com/javarush/task/task07/task0706/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0706/Solution.java
@@ -11,18 +11,38 @@ public class Solution {
         //напишите тут ваш код
         int dom1 = 0;
         int dom2 = 0;
+        int arrLength = 15;
+
         Scanner scan = new Scanner(System.in);
-        int[] list = new int[15];
-        for (int i = 0; i < 15; i++) {
+        int[] list = new int[arrLength];
+
+        for (int i = 0; i < list.length; i++) {
             list[i] = scan.nextInt();
             {
-                if (i % 2 == 0 || i == 0) dom2 = dom2 + list[i];
-                else dom1 = dom1 + list[i];
+                if (i % 2 == 0 || i == 0) {
+                    dom2 = dom2 + list[i];
+                } else {
+                    dom1 = dom1 + list[i];
+                }
             }
         }
-        if (dom1 < dom2)
-            System.out.println("В домах с четными номерами проживает больше жителей.");
-        else System.out.println("В домах с нечетными номерами проживает больше жителей.");
+
+        Print(dom1, dom2);
+//        if (dom1 < dom2) {
+//            System.out.println("В домах с четными номерами проживает больше жителей.");
+//        } else {
+//            System.out.println("В домах с нечетными номерами проживает больше жителей.");
+//        }
+    }
+
+    private static void Print(int dom1, int dom2) {
+        String result = "В домах с нечетными номерами проживает больше жителей.";
+
+        if (dom1 < dom2) {
+            result = "В домах с четными номерами проживает больше жителей.";
+        }
+
+        System.out.println(result);
     }
 }
 
--- 1.JavaSyntax/src/com/javarush/task/task07/task0718/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0718/Solution.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.List;
 
 /* 
 Проверка на упорядоченность
@@ -11,9 +12,14 @@ import java.util.ArrayList;
 public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        ArrayList<String> list = new ArrayList<>();
-        for (int i = 0; i < 10; i++) list.add(reader.readLine());
+        List<String> list = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            list.add(reader.readLine());
+        }
+
         int size = list.get(0).length();
+
         for (int i = 1; i < 10; i++) {
             if (list.get(i).length() >= size) size = list.get(i).length();
             else System.out.println(i);
